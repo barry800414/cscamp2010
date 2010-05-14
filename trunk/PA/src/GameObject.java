fd3dd26dfb64a43f2df8c73d732ef23a707d1660
robertabcd <robertabcd@c3c2d881-88b9-b942-f097-dc5e7f45527e>
2010-05-14 10:01:43 +0000

    GameObject, Player, Bullet: getter & setter implementation

--- trunk/PA/src/GameObject.java
+++ trunk/PA/src/GameObject.java
@@ -1,30 +1,54 @@
 
 public class GameObject {
+	public static final double[] DIRECTION_DEGREE = {0, 45, 90, 135, 180, 225, 270, 315};
+
+	protected Game game;
+	protected double locX, locY, dirX, dirY, speed;
+	
+	public GameObject(Game game) {
+		this.game = game;
+	}
+	
 	public double getLocX() {
+		return locX;
 	}
 	
 	public double getLocY() {
+		return locY;
 	}
 	
 	public double getDirX() {
+		return dirX;
 	}
 	
 	public double getDirY() {
+		return dirY;
 	}
 	
 	public double getSpeed() {
+		return speed;
 	}
 	
 	public void setLoc(double x, double y) {
+		locX = x;
+		locY = y;
 	}
 	
 	public void setDir(double x, double y) {
+		dirX = x;
+		dirY = y;
 	}
 	
 	/** set direction 0~7 */
 	public void setDir(int direct) {
+		if(direct >= 0 && direct <= 7) {
+			double rad = Math.PI * DIRECTION_DEGREE[direct] / 180;
+			dirX = Math.cos(rad);
+			dirY = -Math.sin(rad); // y-coordinate on screen is different
+		}
 	}
 	
 	public void setSpeed(double v) {
+		speed = v;
 	}
 }
