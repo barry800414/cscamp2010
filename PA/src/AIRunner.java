2ad8703137f272cb214ace2b06850e143f336b22
robertabcd <robertabcd@c3c2d881-88b9-b942-f097-dc5e7f45527e>
2010-05-14 04:12:20 +0000

    AIRunner implementation

--- PA/src/AIRunner.java
+++ PA/src/AIRunner.java
@@ -4,24 +4,50 @@
  * which prevents from hanging other parts of program.
  */
 public class AIRunner {
+	private String classname;
+	private AI ai_instance;
+	private Thread ai_thread;
+	private boolean finished;
+	
 	/** Init with class name */
 	public AIRunner(String classname) {
+		try {
+			this.classname = classname;
+			ai_instance = (AI)(Class.forName(classname).newInstance());
+		} catch(ClassNotFoundException e) {
+			System.out.println("AI Class: " + classname + " not found.");
+			e.printStackTrace();
+		} catch(Exception e) {
+			System.out.println("AI Class: " + classname + " unable to load.");
+			e.printStackTrace();
+		}
 	}
 	
 	/** Get the instance of the AI */
 	public AI getAIInstance() {
+		return ai_instance;
 	}
 	
 	/** Returns true if the AI had finished running */
 	public boolean isFinished() {
+		return finished;
 	}
 	
-	/** Suspend AI thread if exist any */
-	public void suspend() {
+	/** The thread will use this method to report its state */
+	public void setFinished(boolean finished) {
+		this.finished = finished;
 	}
 
 	/** Stop AI thread if exist any */
 	public void stop() {
+		if(!finished) {
+			try {
+				ai_thread.interrupt();
+				finished = true;
+			} catch(SecurityException e) {
+				System.out.println("AI Class: " + classname + " access denied and cannot stop.");
+			}
+		}
 	}
 
 	/**
@@ -29,5 +55,24 @@ public class AIRunner {
 	 * NOTE: ONLY when no existing thread running.
 	 */
 	public void run() {
+		if(finished) {
+			try {
+				final AIRunner that = this;
+				ai_thread = new Thread(new Runnable() {
+					public AIRunner parent = that;
+					public AI ai = that.getAIInstance();
+					public void run() {
+						parent.setFinished(false);
+						ai.run();
+						parent.setFinished(true);
+					}
+				});
+				ai_thread.start();
+			} catch(IllegalThreadStateException e) {
+				System.out.println("AI Class: " + classname);
+				e.printStackTrace();
+				finished = true;
+			}
+		}
 	}
 }
