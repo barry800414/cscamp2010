659d8255e1d4c706865198fdfba3f1bdf0a74d0d
robertabcd <robertabcd@c3c2d881-88b9-b942-f097-dc5e7f45527e>
2010-05-14 10:01:43 +0000

    GameObject, Player, Bullet: getter & setter implementation

--- PA/src/Bullet.java
+++ PA/src/Bullet.java
@@ -1,8 +1,16 @@
 
 public class Bullet extends GameObject {
+	private Player owner;
+	
+	public Bullet(Game game) {
+		super(game);
+	}
+	
 	public Player getOwner() {
+		return owner;
 	}
 	
 	public void setOwner(Player p) {
+		owner = p;
 	}
 }
--- PA/src/Player.java
+++ PA/src/Player.java
@@ -1,27 +1,77 @@
+import java.util.Hashtable;
+import java.util.ArrayList;
 
 public class Player extends GameObject {
+	public static final double	BASE_SPEED = 70.0;
+	public static final int		BASE_LIFE = 3;
+	
+	private int							life;
+	private boolean						silence;
+	private Hashtable<Skill, Integer>	skill_quota;
+	private ArrayList<Effect>			effects;
+	
+	public Player(Game game) {
+		super(game);
+		
+		life = BASE_LIFE;
+		silence = false;
+		skill_quota = new Hashtable<Skill, Integer>();
+		effects = new ArrayList<Effect>();
+		
+		updateStatus();
+	}
+	
 	public int getLife() {
+		return life;
 	}
 	
 	public boolean isSilence() {
+		return silence;
 	}
 	
 	public void setSilence(boolean silence) {
+		this.silence = silence;
 	}
 	
 	public void useSkill(Skill skill, Player target) {
+		if(!isSilence()) {
+			int quota = getSkillQuota(skill);
+			if(quota > 0) {
+				if(!skill.canSetTarget()) target = this;
+				skill.use(game, this, target);
+				setSkillQuota(skill, quota - 1);
+			} else {
+				System.out.println("Player: " + this + " don't have skill " + skill + ".");
+			}
+		}
 	}
 	
 	public void setSkillQuota(Skill skill, int quota) {
+		skill_quota.put(skill, quota);
 	}
 	
 	public int getSkillQuota(Skill skill) {
+		Integer quota = skill_quota.get(skill);
+		if(quota == null)
+			return 0;
+		else
+			return quota.intValue();
 	}
 	
 	public void addEffect(Effect effect) {	
+		effects.add(effect);
 	}
 	
 	public void removeEffect(Effect effect){
+		effects.remove(effect);
+	}
+	
+	public void applyDamage(Damage damage) {
+		for(Effect eff : effects) {
+			eff.onDamage(damage);
+		}
+		
+		life += damage.life;
 	}
 	
 	/**
@@ -30,5 +80,11 @@ public class Player extends GameObject {
 	 * And iterate all effects and invoke onEffect().
 	 */
 	public void updateStatus() {
+		// Reset information
+		setSpeed(BASE_SPEED);
+		
+		for(Effect eff : effects) {
+			eff.onEffect(this);
+		}
 	}
 }
