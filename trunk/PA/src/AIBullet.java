28c8122f25dbb118bce2230968037bce7ef05828
robertabcd <robertabcd@c3c2d881-88b9-b942-f097-dc5e7f45527e>
2010-06-24 00:12:29 +0000

    AIBullet: speedX,speedY type changed to double.

--- trunk/PA/src/AIBullet.java
+++ trunk/PA/src/AIBullet.java
@@ -1,13 +1,14 @@
 
 public final class AIBullet {
-	public int locX, locY, direct, speedX, speedY, owner;
+	public int locX, locY, direct, owner;
+	public double speedX, speedY;
 	
 	public AIBullet(Bullet bullet) {
 		locX = (int)Math.round(bullet.getLocX());
 		locY = (int)Math.round(bullet.getLocY());
 		direct = 1 + (int)Math.round(bullet.getDirectRad() * 180.0 / Math.PI);
-		speedX = (int)Math.round(bullet.getSpeed() * bullet.getDirX());
-		speedY = (int)Math.round(bullet.getSpeed() * bullet.getDirY());
+		speedX = bullet.getSpeed() * bullet.getDirX();
+		speedY = bullet.getSpeed() * bullet.getDirY();
 		
 		Player bullet_owner = bullet.getOwner();
 		owner = (bullet_owner == null) ? 0 : bullet_owner.getId();
