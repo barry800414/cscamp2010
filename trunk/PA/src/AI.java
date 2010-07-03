2b3e311ffbe7b7b1acc17017ac344d68b16fc775
robertabcd <robertabcd@c3c2d881-88b9-b942-f097-dc5e7f45527e>
2010-07-03 16:28:39 +0000

    AI: temp. fix.

--- trunk/PA/src/AI.java
+++ trunk/PA/src/AI.java
@@ -16,6 +16,10 @@ public abstract class AI {
 	private Skill skill;
 	private int target;
 	
+	public AI() {
+		resetInternalState();
+	}
+	
 	public final void resetInternalState() {
 		players.clear();
 		bullets = new AIBullet[0];
